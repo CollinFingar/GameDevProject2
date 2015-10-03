75b5a1dcdc6df98da6e2c7b2335a931faf3733a7
Collin Fingar <fingac@rpi.edu>
2015-10-03 18:22:29 -0400

    added and implemented the 'idle' animation.

--- Project2/source/Player.hx
+++ Project2/source/Player.hx
@@ -22,27 +22,29 @@ class Player extends FlxSprite
     public function new(X:Float=0, Y:Float=0, Parent:PlayState) 
     {
         super(X, Y);
-        makeGraphic(80, 120, FlxColor.CRIMSON);
-        //loadGraphic("assets/images/linda.png", true, 16, 16);
+        //makeGraphic(80, 120, FlxColor.CRIMSON);
+        loadGraphic("assets/images/damsel/princess_blink1_307x343_8fps_strip8.png", true, 307, 343);
         //animation.add("walk", [4, 5, 6, 7], 12, true);
-        //animation.add("idle", [5]);
+        animation.add("idle", [0,1,2,3,4,5,6,7], 8, true);
         drag.set(RUN_SPEED * 8, RUN_SPEED * 8);
         maxVelocity.set(RUN_SPEED * 2, RUN_SPEED * 6);
         acceleration.y = 1500;
         parent = Parent;
-        //scale.set(2, 2);
-        updateHitbox();
+		scale.set(.5, .5);
+        setSize(width / 4, height / 3);
+		offset.set(width*1.5, height);
+        //updateHitbox();
     }
     
     public override function update():Void {
         acceleration.x = 0;
         if (FlxG.keys.anyPressed(["LEFT", "A"])) {
             acceleration.x = -drag.x;
-            flipX = true;
+            flipX = false;
         }
         if (FlxG.keys.anyPressed(["RIGHT", "D"])) {
             acceleration.x = drag.x;
-            flipX = false;
+            flipX = true;
         }
         if(FlxG.keys.anyJustPressed(["SPACE"])){
             if(!playerJumping && velocity.y == 0){
@@ -53,21 +55,23 @@ class Player extends FlxSprite
         }
         if (velocity.x > 0 || velocity.x < 0) {
 			if(velocity.x > 0){
-				makeGraphic(80, 120, FlxColor.GREEN);
+				//makeGraphic(80, 120, FlxColor.GREEN);
 			} else {
-				makeGraphic(80, 120, FlxColor.YELLOW);
+				//makeGraphic(80, 120, FlxColor.YELLOW);
 			}
             //animation.play("walk");
         }
         else {
-			makeGraphic(80, 120, FlxColor.CRIMSON);
-            //animation.play("idle");
+			//makeGraphic(80, 120, FlxColor.CRIMSON);
+            animation.play("idle");
+			
+			//offset.set(width/2, height/2 + 10);
         }
 		
 		if(velocity.y <= 0 && playerJumping){
-			makeGraphic(80, 120, FlxColor.WHITE);
+			//makeGraphic(80, 120, FlxColor.WHITE);
 		} else if(velocity.y > 0 && playerJumping){
-			makeGraphic(80, 120, FlxColor.BLACK);
+			//makeGraphic(80, 120, FlxColor.BLACK);
 		}
         super.update();
     }
